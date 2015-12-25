b1524efaf06f37f5faefd891b2cdc0a6a9d827b1
Matthew Garrett <mjg59@coreos.com>
2015-12-25 11:30:14 +0000

    Add bind/seal/unbind/unseal support
    TPMs can encrypt material, optionally tying it to the state of a set of PCRs
    in the process. Add support for that.

--- tspi/key.go
+++ tspi/key.go
@@ -76,6 +76,119 @@ func (key *Key) GetKeyBlob() ([]byte, error) {
 	return data, err
 }
 
+// Bind encrypts some data using the TPM and returns it.
+func (key *Key) Bind(data []byte) ([]byte, error) {
+	var encdata C.TSS_HENCDATA
+	var dataLen C.UINT32
+	var cData *C.BYTE
+
+	err := tspiError(C.Tspi_Context_CreateObject(key.context, C.TSS_OBJECT_TYPE_ENCDATA, C.TSS_ENCDATA_BIND, (*C.TSS_HOBJECT)(&encdata)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_Data_Bind(encdata, key.handle, (C.UINT32)(len(data)), (*C.BYTE)(&data[0])))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_GetAttribData((C.TSS_HOBJECT)(encdata), C.TSS_TSPATTRIB_ENCDATA_BLOB, C.TSS_TSPATTRIB_ENCDATABLOB_BLOB, &dataLen, &cData))
+	if err != nil {
+		return nil, err
+	}
+
+	blob := C.GoBytes(unsafe.Pointer(cData), (C.int(dataLen)))
+	C.Tspi_Context_FreeMemory(key.context, cData)
+	return blob, nil
+}
+
+// Unbind decrypts data previously encrypted with this key
+func (key *Key) Unbind(data []byte) ([]byte, error)  {
+	var encdata C.TSS_HENCDATA
+	var dataLen C.UINT32
+	var cData *C.BYTE
+
+	err := tspiError(C.Tspi_Context_CreateObject(key.context, C.TSS_OBJECT_TYPE_ENCDATA, C.TSS_ENCDATA_BIND, (*C.TSS_HOBJECT)(&encdata)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_SetAttribData((C.TSS_HOBJECT)(encdata), C.TSS_TSPATTRIB_ENCDATA_BLOB, C.TSS_TSPATTRIB_ENCDATABLOB_BLOB, (C.UINT32)(len(data)), (*C.BYTE)(unsafe.Pointer(&data[0]))))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_Data_Unbind(encdata, key.handle, &dataLen, &cData))
+	if err != nil {
+		return nil, err
+	}
+
+	blob := C.GoBytes(unsafe.Pointer(cData), (C.int(dataLen)))
+	C.Tspi_Context_FreeMemory(key.context, cData)
+	return blob, nil
+}
+
+// Seal encrypts the data using the TPM such that it can only be decrypted
+// when the TPM's PCR values match the values set on the provided PCRs
+// object. If pcrs is nil, the data will be sealed to the TPM but may be
+// decrypted regardless of platform state.
+func (key *Key) Seal(data []byte, pcrs *PCRs) ([]byte, error) {
+	var encdata C.TSS_HENCDATA
+	var dataLen C.UINT32
+	var cData *C.BYTE
+	var pcrhandle C.TSS_HPCRS
+
+	if (pcrs != nil) {
+		pcrhandle = pcrs.handle
+	}
+
+	err := tspiError(C.Tspi_Context_CreateObject(key.context, C.TSS_OBJECT_TYPE_ENCDATA, C.TSS_ENCDATA_SEAL, (*C.TSS_HOBJECT)(&encdata)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_Data_Seal(encdata, key.handle, (C.UINT32)(len(data)), (*C.BYTE)(&data[0]), pcrhandle))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_GetAttribData((C.TSS_HOBJECT)(encdata), C.TSS_TSPATTRIB_ENCDATA_BLOB, C.TSS_TSPATTRIB_ENCDATABLOB_BLOB, &dataLen, &cData))
+	if err != nil {
+		return nil, err
+	}
+
+	blob := C.GoBytes(unsafe.Pointer(cData), (C.int(dataLen)))
+	C.Tspi_Context_FreeMemory(key.context, cData)
+	return blob, nil
+}
+
+// Unseal decrypts data previously encrypted with this key as long as the
+// PCR values match those the data was sealed against
+func (key *Key) Unseal(data []byte) ([]byte, error)  {
+	var encdata C.TSS_HENCDATA
+	var dataLen C.UINT32
+	var cData *C.BYTE
+
+	err := tspiError(C.Tspi_Context_CreateObject(key.context, C.TSS_OBJECT_TYPE_ENCDATA, C.TSS_ENCDATA_SEAL, (*C.TSS_HOBJECT)(&encdata)))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_SetAttribData((C.TSS_HOBJECT)(encdata), C.TSS_TSPATTRIB_ENCDATA_BLOB, C.TSS_TSPATTRIB_ENCDATABLOB_BLOB, (C.UINT32)(len(data)), (*C.BYTE)(unsafe.Pointer(&data[0]))))
+	if err != nil {
+		return nil, err
+	}
+
+	err = tspiError(C.Tspi_Data_Unseal(encdata, key.handle, &dataLen, &cData))
+	if err != nil {
+		return nil, err
+	}
+
+	blob := C.GoBytes(unsafe.Pointer(cData), (C.int(dataLen)))
+	C.Tspi_Context_FreeMemory(key.context, cData)
+	return blob, nil
+}
+
 // GenerateKey generates a key pair on the TPM, wrapping it with the provided
 // key
 func (key *Key) GenerateKey(wrapkey *Key) (err error) {
