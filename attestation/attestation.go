787ebe9c349896754c25188ec7d06c538d946a3f
Denis Karch <dkarch@google.com>
2019-03-06 10:13:13 -0800

    Modified CreateAIK to return the *tspi.Key as well as the blobs
    This way we do not have to load the key from the blob in order
    to use it.
    Also modified error checking to be of the form err = ...; if err != nil

--- attestation/attestation.go
+++ attestation/attestation.go
@@ -73,9 +73,9 @@ func AIKChallengeResponse(context *tspi.Context, aikblob []byte, asymchallenge [
 }
 
 // CreateAIK asks the TPM to generate an Attestation Identity Key. It returns
-// the unencrypted public half of the AIK along with an encrypted blob
-// containing both halves of the key, and any error.
-func CreateAIK(context *tspi.Context) ([]byte, []byte, error) {
+// the tspi.Key struct the unencrypted public half of the AIK along with an
+// encrypted blob containing both halves of the key, and any error.
+func CreateAIK(context *tspi.Context) (*tspi.Key, []byte, []byte, error) {
 	var wellKnown [20]byte
 	n := make([]byte, 2048/8)
 	for i := 0; i < 2048/8; i++ {
@@ -84,61 +84,54 @@ func CreateAIK(context *tspi.Context) ([]byte, []byte, error) {
 
 	srk, err := context.LoadKeyByUUID(tspiconst.TSS_PS_TYPE_SYSTEM, tspi.TSS_UUID_SRK)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 	keypolicy, err := srk.GetPolicy(tspiconst.TSS_POLICY_USAGE)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-	err = keypolicy.SetSecret(tspiconst.TSS_SECRET_MODE_SHA1, wellKnown[:])
-	if err != nil {
-		return nil, nil, err
+	if err = keypolicy.SetSecret(tspiconst.TSS_SECRET_MODE_SHA1, wellKnown[:]); err != nil {
+		return nil, nil, nil, err
 	}
 
 	tpm := context.GetTPM()
 	tpmpolicy, err := tpm.GetPolicy(tspiconst.TSS_POLICY_USAGE)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
-	err = tpm.AssignPolicy(tpmpolicy)
-	if err != nil {
-		return nil, nil, err
-	}
-	err = tpmpolicy.SetSecret(tspiconst.TSS_SECRET_MODE_SHA1, wellKnown[:])
-	if err != nil {
-		return nil, nil, err
+	if err = tpmpolicy.SetSecret(tspiconst.TSS_SECRET_MODE_SHA1, wellKnown[:]); err != nil {
+		return nil, nil, nil, err
 	}
 
 	pcakey, err := context.CreateKey(tspiconst.TSS_KEY_TYPE_LEGACY | tspiconst.TSS_KEY_SIZE_2048)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
-	err = pcakey.SetModulus(n)
-	if err != nil {
-		return nil, nil, err
+	if err = pcakey.SetModulus(n); err != nil {
+		return nil, nil, nil, err
 	}
 
 	aik, err := context.CreateKey(tspiconst.TSS_KEY_TYPE_IDENTITY | tspiconst.TSS_KEY_SIZE_2048)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 	_, err = tpm.CollateIdentityRequest(srk, pcakey, aik)
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
 	pubkey, err := aik.GetPubKeyBlob()
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 	blob, err := aik.GetKeyBlob()
 	if err != nil {
-		return nil, nil, err
+		return nil, nil, nil, err
 	}
 
 	_, err = aik.GetModulus()
-	return pubkey, blob, nil
+	return aik, pubkey, blob, nil
 }
 
 // GetEKCert reads the Endorsement Key certificate from the TPM's NVRAM and
